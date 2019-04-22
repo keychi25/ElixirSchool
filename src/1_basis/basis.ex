23684bbf4f55f03b8c5e1158c696dccb00f0e5e5
keychi25 <e165412y@st.u-gakugei.ac.jp>
2019-04-22 22:49:51 +0900

    ElixirSchool 1

---
+++ src/1_basis/basis.ex
@@ -0,0 +1,47 @@
+defmodule Basis do
+  @moduledoc false
+
+ def sum(x, y) do
+   IO.puts x + y
+  end
+  sum = fn (x, y) ->
+    IO.puts x + y
+  end
+   sum.(2, 3)
+
+  atom = fn () ->
+    IO. puts true |> is_atom
+    IO. puts :true |> is_boolean
+    IO. puts :true === true
+
+  end
+  atom.()
+
+  logic = fn () ->
+    IO.puts -20 || true
+    IO.puts false || 42
+
+    IO.puts 42 && true
+    IO.puts 42 && nil
+
+    IO.puts !42
+    IO.puts !false
+  end
+  logic.()
+
+  # ソートされる順序
+  # number < atom < reference < function < port < pid < tuple < map < list < bitstring
+
+  # 文字列の式展開
+  string = fn (name)->
+    IO.puts "Hello #{name} !!"
+  end
+  string.(Elixir)
+
+  # 文字列の連結
+  link = fn (name) ->
+   IO.puts  "Hello " <> name
+  end
+  link.("elixir")
+  link.("phoenix")
+end
